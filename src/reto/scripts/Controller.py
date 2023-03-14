592f2fcf2e1e4c88f81bf8461b263e78b580093c
Esteb37 <esteban37padilla@gmail.com>
2023-03-13 23:25:01 -0600

    Linting

--- src/reto/scripts/Controller.py
+++ src/reto/scripts/Controller.py
@@ -12,39 +12,39 @@ motor_output = 0
 setpoint = 0
 mode = 0
 
-TS = rospy.get_param("/controller_Ts")
-KP_STEP = rospy.get_param("/controller_kP_step")
-KP_STEP = rospy.get_param("/controller_kP_step")
-KI_STEP = rospy.get_param("/controller_kI_step")
-KD_STEP = rospy.get_param("/controller_kD_step")
+TS = rospy.get_param("/controller_Ts",0)
+KP_STEP = rospy.get_param("/controller_kP_step",0)
+KP_STEP = rospy.get_param("/controller_kP_step",0)
+KI_STEP = rospy.get_param("/controller_kI_step",0)
+KD_STEP = rospy.get_param("/controller_kD_step",0)
 
-KP_SINE = rospy.get_param("/controller_kP_sine")
-KI_SINE = rospy.get_param("/controller_kI_sine")
-KD_SINE = rospy.get_param("/controller_kD_sine")
+KP_SINE = rospy.get_param("/controller_kP_sine",0)
+KI_SINE = rospy.get_param("/controller_kI_sine",0)
+KD_SINE = rospy.get_param("/controller_kD_sine",0)
 
-KP_SQUARE = rospy.get_param("/controller_kP_square")
-KI_SQUARE = rospy.get_param("/controller_kI_square")
-KD_SQUARE = rospy.get_param("/controller_kD_square")
+KP_SQUARE = rospy.get_param("/controller_kP_square",0)
+KI_SQUARE = rospy.get_param("/controller_kI_square",0)
+KD_SQUARE = rospy.get_param("/controller_kD_square",0)
 
 e = [0,0,0]
 u = [0,0]
 
 #Stop Condition
 def stop():
- #Setup the stop message (can be the same as the control message)
-  print("Stopping")
+    #Setup the stop message (can be the same as the control message)
+    print("Stopping")
 
 def callback_motor_output(msg_motor_output):
-  rospy.loginfo("RPS: {}".format(msg_motor_output.data))
-  global motor_output
-  motor_output = msg_motor_output.data
+    rospy.loginfo("RPS: {}".format(msg_motor_output.data))
+    global motor_output
+    motor_output = msg_motor_output.data
 
 def callback_setpoint(msg_setpoint):
-  rospy.loginfo("Setpoint: {}".format(msg_setpoint.value))
-  global setpoint
-  global mode
-  setpoint = msg_setpoint.value
-  mode =  msg_setpoint.mode
+    rospy.loginfo("Setpoint: {}".format(msg_setpoint.value))
+    global setpoint
+    global mode
+    setpoint = msg_setpoint.value
+    mode =  msg_setpoint.mode
 
 if __name__=='__main__':
     #Initialise and Setup node
@@ -56,18 +56,18 @@ if __name__=='__main__':
     pub = rospy.Publisher("/motor_input", Float32, queue_size=10)
 
     print("The Controller is Running")
-    
+
     #Run the node
     while not rospy.is_shutdown():
-    
+
         kP,kI,kD = [(KP_STEP, KI_STEP, KD_STEP),
                     (KP_SINE, KI_SINE, KD_SINE),
-                    (KP_SQUARE, KI_SQUARE, KD_SQUARE)][mode] 
-            
-        K1 = kP + TS * kI + kD / TS;
-        K2 = -kP - 2.0 * kD / TS;
-        K3 = kD / TS;
-        
+                    (KP_SQUARE, KI_SQUARE, KD_SQUARE)][mode]
+
+        K1 = kP + TS * kI + kD / TS
+        K2 = -kP - 2.0 * kD / TS
+        K3 = kD / TS
+
         vel = motor_output
 
         e[0] = setpoint - vel
@@ -77,13 +77,10 @@ if __name__=='__main__':
         e[2] = e[1]
         e[1] = e[0]
         u[1] = u[0]
-        
+
         result = u[0]
-  
-        if result < -1:
-            result = -1
-        elif result > 1:
-            result = 1
-            
+
+        result = np.clip(result, -1, 1)
+
         pub.publish(result)
         rate.sleep()
--- src/reto/scripts/Setpoint.py
+++ src/reto/scripts/Setpoint.py
@@ -1,19 +1,18 @@
 #!/usr/bin/env python
 import rospy
 import numpy as np
-from std_msgs.msg import Float32
 from reto.msg import set_point
 
-AMPLITUDE = rospy.get_param("/setpoint_amplitude")
-MAX_AMPLITUDE = rospy.get_param("/setpoint_max_amplitude")
-PERIOD = rospy.get_param("/setpoint_period")
-MODE = rospy.get_param("/setpoint_mode")
+AMPLITUDE = rospy.get_param("/setpoint_amplitude",0)
+MAX_AMPLITUDE = rospy.get_param("/setpoint_max_amplitude",0)
+PERIOD = rospy.get_param("/setpoint_period",0)
+MODE = rospy.get_param("/setpoint_mode",0)
 
 
 #Stop Condition
 def stop():
- #Setup the stop message (can be the same as the control message)
-  print("Stopping")
+    #Setup the stop message (can be the same as the control message)
+    print("Stopping")
 
 
 if __name__=='__main__':
@@ -25,32 +24,29 @@ if __name__=='__main__':
     pub = rospy.Publisher("/set_point", set_point, queue_size=10)
 
     print("The Setpoint is Running")
-    
+
     time = rospy.get_time()
-    
+
     #Run the node
     while not rospy.is_shutdown():
-        
+
         t = rospy.get_time() - time
-        
-        
+
         if MODE == 0:
             setpoint = input("Provide speed in Radians per Second from -{0} RPS to {0} RPS: ".format(MAX_AMPLITUDE))
             setpoint = np.clip(setpoint, -MAX_AMPLITUDE, MAX_AMPLITUDE)
-            
+
         elif MODE == 1:
             setpoint = np.sin(t*2*np.pi/PERIOD)*AMPLITUDE
-            
-        elif MODE == 2:
-            setpoint = np.sin(t*2*np.pi/PERIOD)*AMPLITUDE    
-            setpoint = np.sign(setpoint) * AMPLITUDE
-            
 
+        elif MODE == 2:
+            setpoint = np.sin(t*2*np.pi/PERIOD)*AMPLITUDE
+            setpoint = np.sign(setpoint) * AMPLITUDE
 
         else:
             print("Mode {} is not allowed.".format(MODE))
             break
-            
+
         pub.publish(setpoint, MODE)
         rospy.loginfo(setpoint)
         rate.sleep()
