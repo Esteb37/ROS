1036d76ff9c77a395f0a2561003efdc2b678d3e7
esteb37 <esteban37padilla@gmail.com>
2023-05-02 09:20:10 -0600

    Gazebo color filter

--- src/cv_practise/src/filter.py
+++ src/cv_practise/src/filter.py
@@ -6,12 +6,63 @@ import rospy
 import cv2
 import numpy as np
 
-min_green = np.array([50, 220, 220])
-max_green = np.array([60, 255, 255])
-min_red = np.array([170, 220, 220])
-max_red = np.array([180, 255, 255])
-min_blue = np.array([110, 220, 220])
-max_blue = np.array([120, 255, 255])
+H_min = 0  # HUE (0-180) = (0-360)
+S_min = 0  # SATURATION (0-255)
+V_min = 0  # VALUE (0-255)
+
+H_max = 180
+S_max = 255
+V_max = 255
+
+
+def save_value(name, value):
+    with open("/home/estebanp/catkin_ws/src/cv_practise/src/hsv_values.txt", "r+") as f:
+        # Find the line and replace it with the new value
+        lines = f.readlines()
+        for i, line in enumerate(lines):
+            if name in line:
+                lines[i] = line.replace(line.split(":")[1], str(value)+"\n")
+
+        f.seek(0)
+        f.truncate()
+        f.writelines(lines)
+
+
+def get_value(name):
+    try:
+        with open("/home/estebanp/catkin_ws/src/cv_practise/src/hsv_values.txt", "r") as f:
+            # Find the line with the name and read the value
+            lines = f.readlines()
+            for line in lines:
+                if name in line:
+                    return int(line.split(":")[1])
+    except:
+        return 0
+
+
+cv2.namedWindow("Sliders")
+cv2.resizeWindow("Sliders", 500, 200)
+
+cv2.createTrackbar("H Min", "Sliders", 0, 180,
+                   lambda x: save_value("H_min", x))
+cv2.setTrackbarPos("H Min", "Sliders", get_value("H_min"))
+cv2.createTrackbar("H Max", "Sliders", 0, 180,
+                   lambda x: save_value("H_max", x))
+cv2.setTrackbarPos("H Max", "Sliders", get_value("H_max"))
+
+cv2.createTrackbar("S Min", "Sliders", 0, 255,
+                   lambda x: save_value("S_min", x))
+cv2.setTrackbarPos("S Min", "Sliders", get_value("S_min"))
+cv2.createTrackbar("S Max", "Sliders", 0, 255,
+                   lambda x: save_value("S_max", x))
+cv2.setTrackbarPos("S Max", "Sliders", get_value("S_max"))
+
+cv2.createTrackbar("V Min", "Sliders", 0, 255,
+                   lambda x: save_value("V_min", x))
+cv2.setTrackbarPos("V Min", "Sliders", get_value("V_min"))
+cv2.createTrackbar("V Max", "Sliders", 0, 255,
+                   lambda x: save_value("V_max", x))
+cv2.setTrackbarPos("V Max", "Sliders", get_value("V_max"))
 
 
 class Filter(object):
@@ -28,11 +79,18 @@ class Filter(object):
 
         while not rospy.is_shutdown():
             if self.img_received:
-                cv2.imshow('image', self.cv_image)
+
+                H_min = cv2.getTrackbarPos("H Min", "Sliders")
+                H_max = cv2.getTrackbarPos("H Max", "Sliders")
+                S_min = cv2.getTrackbarPos("S Min", "Sliders")
+                S_max = cv2.getTrackbarPos("S Max", "Sliders")
+                V_min = cv2.getTrackbarPos("V Min", "Sliders")
+                V_max = cv2.getTrackbarPos("V Max", "Sliders")
 
                 image = cv2.resize(self.cv_image, (300, 300))
                 hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-                mask_r = cv2.inRange(hsv, min_red, max_red)
+                mask_r = cv2.inRange(
+                    hsv, (H_min, S_min, V_min), (H_max, S_max, V_max))
 
                 res_r = cv2.bitwise_and(image, image, mask=mask_r)
                 cv2.imshow('Original', image)
