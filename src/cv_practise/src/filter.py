7e9c31941ddbf3b394e3f978be8017488997e453
esteb37 <esteban37padilla@gmail.com>
2023-05-02 19:44:53 -0600

    Removed sliders for filter

--- src/cv_practise/src/filter.py
+++ src/cv_practise/src/filter.py
@@ -6,63 +6,12 @@ import rospy
 import cv2
 import numpy as np
 
-H_min = 0  # HUE (0-180) = (0-360)
-S_min = 0  # SATURATION (0-255)
-V_min = 0  # VALUE (0-255)
-
-H_max = 180
+H_min = 0
+H_max = 22
+S_min = 1
 S_max = 255
+V_min = 35
 V_max = 255
-
-
-def save_value(name, value):
-    with open("/home/estebanp/catkin_ws/src/cv_practise/src/hsv_values.txt", "r+") as f:
-        # Find the line and replace it with the new value
-        lines = f.readlines()
-        for i, line in enumerate(lines):
-            if name in line:
-                lines[i] = line.replace(line.split(":")[1], str(value)+"\n")
-
-        f.seek(0)
-        f.truncate()
-        f.writelines(lines)
-
-
-def get_value(name):
-    try:
-        with open("/home/estebanp/catkin_ws/src/cv_practise/src/hsv_values.txt", "r") as f:
-            # Find the line with the name and read the value
-            lines = f.readlines()
-            for line in lines:
-                if name in line:
-                    return int(line.split(":")[1])
-    except:
-        return 0
-
-
-cv2.namedWindow("Sliders")
-cv2.resizeWindow("Sliders", 500, 200)
-
-cv2.createTrackbar("H Min", "Sliders", 0, 180,
-                   lambda x: save_value("H_min", x))
-cv2.setTrackbarPos("H Min", "Sliders", get_value("H_min"))
-cv2.createTrackbar("H Max", "Sliders", 0, 180,
-                   lambda x: save_value("H_max", x))
-cv2.setTrackbarPos("H Max", "Sliders", get_value("H_max"))
-
-cv2.createTrackbar("S Min", "Sliders", 0, 255,
-                   lambda x: save_value("S_min", x))
-cv2.setTrackbarPos("S Min", "Sliders", get_value("S_min"))
-cv2.createTrackbar("S Max", "Sliders", 0, 255,
-                   lambda x: save_value("S_max", x))
-cv2.setTrackbarPos("S Max", "Sliders", get_value("S_max"))
-
-cv2.createTrackbar("V Min", "Sliders", 0, 255,
-                   lambda x: save_value("V_min", x))
-cv2.setTrackbarPos("V Min", "Sliders", get_value("V_min"))
-cv2.createTrackbar("V Max", "Sliders", 0, 255,
-                   lambda x: save_value("V_max", x))
-cv2.setTrackbarPos("V Max", "Sliders", get_value("V_max"))
 
 
 class Filter(object):
@@ -75,17 +24,14 @@ class Filter(object):
         r = rospy.Rate(10)  # 10Hz
         self.image_sub = rospy.Subscriber(
             "camera/image_raw", Image, self.camera_callback)
+
+        self.mask_pub = rospy.Publisher(
+            "/segmented_image", Image, queue_size=1)
+
         self.img_received = 0
 
         while not rospy.is_shutdown():
             if self.img_received:
-
-                H_min = cv2.getTrackbarPos("H Min", "Sliders")
-                H_max = cv2.getTrackbarPos("H Max", "Sliders")
-                S_min = cv2.getTrackbarPos("S Min", "Sliders")
-                S_max = cv2.getTrackbarPos("S Max", "Sliders")
-                V_min = cv2.getTrackbarPos("V Min", "Sliders")
-                V_max = cv2.getTrackbarPos("V Max", "Sliders")
 
                 image = cv2.resize(self.cv_image, (300, 300))
                 hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
@@ -95,6 +41,11 @@ class Filter(object):
                 res_r = cv2.bitwise_and(image, image, mask=mask_r)
                 cv2.imshow('Original', image)
                 cv2.imshow('Red', res_r)
+
+                image_message = self.bridge_object.cv2_to_imgmsg(
+                    res_r, encoding="passthrough")
+
+                self.mask_pub.publish(image_message)
 
             cv2.waitKey(1)
             r.sleep()
