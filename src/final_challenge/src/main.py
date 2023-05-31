26ee0784fbcdbb61f5c31a86e7cc32df09f3b72c
Esteb37 <esteban37padilla@gmail.com>
2023-05-30 18:52:36 -0600

    Final challenge skeleton

---
+++ src/final_challenge/src/main.py
@@ -0,0 +1,160 @@
+#!/usr/bin/env python
+
+from std_msgs.msg import Float32, Bool, String
+from geometry_msgs.msg import Twist, Pose
+import numpy as np
+import rospy
+from time import time
+
+
+class Robot():
+
+    LINEAR_VELOCITY = 0.5
+    TRACK_WIDTH = 0.19
+    WHEEL_RADIUS = 0.05
+
+    def __init__(self):
+
+        self.linear_vel = 0
+        self.angular_vel = 0
+
+        self.line_angular_vel = 0
+        self.turn_angular_vel = 0
+
+        self.wl = 0
+        self.wr = 0
+        self.heading = 0
+
+        self.traffic_light_status = "none"
+        self.is_stopped = False
+        self.turning = False
+
+        self.init_time = rospy.get_time()
+
+        self.setup_node()
+
+        print("Running...")
+
+        while not rospy.is_shutdown():
+            self.follow_line()
+            self.rate.sleep()
+
+    def setup_node(self):
+        rospy.on_shutdown(self.cleanup)
+        self.rate = rospy.Rate(50)
+        self.setup_publishers()
+        self.setup_subscribers()
+
+        # Setup ROS node
+        print("Waiting for time to be set...")
+        while rospy.get_time() == 0:
+            pass
+
+    def setup_publishers(self):
+        self.cmd_vel_pub = rospy.Publisher(
+            '/cmd_vel', Twist, queue_size=1)
+        self.turn_error_pub = rospy.Publisher(
+            '/turn_error', Float32, queue_size=1)
+
+    def setup_subscribers(self):
+        rospy.Subscriber("/wl", Float32, self.wl_cb)
+        rospy.Subscriber("/wr", Float32, self.wr_cb)
+        rospy.Subscriber("/traffic_light", String, self.traffic_light_cb)
+        rospy.Subscriber("/line_angular_vel", Float32, self.line_angular_vel_cb)
+        rospy.Subscriber("/turn_angular_vel", Float32, self.turn_angular_vel_cb)
+        #rospy.Subscriber("/linear_vel", Float32, self.linear_vel_cb)
+
+    def follow_line(self):
+        if self.is_stopped:
+
+            self.publish_vel(0, 0)
+
+            if self.traffic_light_status == "green":
+                self.publish_vel(self.LINEAR_VELOCITY, self.line_angular_vel)
+                self.is_stopped = False
+
+        else:
+            if self.traffic_light_status == "red":
+                self.publish_vel(0, 0)
+                self.is_stopped = True
+
+            elif self.traffic_light_status == "yellow":
+                self.publish_vel(self.LINEAR_VELOCITY/2, self.line_angular_vel)
+
+            else:
+                self.publish_vel(self.LINEAR_VELOCITY, self.line_angular_vel)
+
+    def update_heading(self):
+        """
+            Updates the current heading of the robot based on the current wheel velocities.
+        """
+
+        dt = rospy.get_time()-self.init_time
+
+        angular_vel = self.WHEEL_RADIUS * \
+            (self.wr-self.wl)/self.TRACK_WIDTH
+
+        theta = angular_vel * dt
+
+        self.heading += np.arctan2(
+            np.sin(theta), np.cos(theta))
+
+        self.init_time = rospy.get_time()
+
+    def turn(self, angle):
+        """
+            Turns the robot by a given angle.
+        """
+        if not self.turning:
+            self.heading = 0
+            self.turning = True
+
+        self.update_heading()
+        self.turn_error_pub.publish(angle - self.heading)
+        self.publish_vel(0, self.turn_angular_vel)
+
+        if np.abs(angle - self.heading) < 0.1:
+            self.turning = False
+            return True
+
+        return False
+
+    def publish_vel(self, linear, angular):
+        """
+            Sends a velocity command to the robot through the cmd_vel topic.
+        """
+        vel = Twist()
+        vel.linear.x = linear
+        vel.angular.z = np.clip(angular, -5, 5)
+        self.cmd_vel_pub.publish(vel)
+
+    def cleanup(self):
+        """
+            Stops the robot when the program is interrupted.
+        """
+        zero_vel = Twist()
+        self.cmd_vel_pub.publish(zero_vel)
+
+        print("My battery is low and it's getting dark")
+
+    def wl_cb(self, wl):
+        self.wl = wl.data
+
+    def wr_cb(self, wr):
+        self.wr = wr.data
+
+    def traffic_light_cb(self, msg):
+        self.traffic_light_status = msg.data
+
+    def line_angular_vel_cb(self, msg):
+        self.line_angular_vel = msg.data
+        print(msg.data)
+
+    def turn_angular_vel_cb(self, msg):
+        self.turn_angular_vel = msg.data
+
+
+############################### MAIN PROGRAM ####################################
+if __name__ == "__main__":
+    rospy.init_node("final_challenge", anonymous=True)
+    Robot()
---
+++ src/final_challenge/src/sign_detector.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+
+# import ROS stuff
+import rospy
+from std_msgs.msg import String
+from geometry_msgs.msg import Point
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+# import the necessary packages
+from collections import deque
+import cv2
+import rospy
+import numpy as np
+
+
+class SignDetector():
+    def __init__(self):
+        rospy.on_shutdown(self.cleanup)
+        ros_rate = rospy.Rate(50)
+
+        self.sign_pub = rospy.Publisher(
+            "/sign", String, queue_size=1)
+        self.image_pub = rospy.Publisher(
+            "/processed_image_sign", Image, queue_size=1)
+
+        self.image_sub = rospy.Subscriber(
+            "/camera/image_raw", Image, self.camera_callback)
+        self.image_sub_jetson = rospy.Subscriber(
+            "/video_source/raw", Image, self.camera_callback)
+        self.image_sub_jetson_launch = rospy.Subscriber(
+            "/camera/video_source/raw", Image, self.camera_callback)
+
+        self.bridge_object = CvBridge()
+
+        # This flag is to ensure we received at least one self.frame
+        self.image_received_flag = 0
+
+        while not rospy.is_shutdown():
+
+            if self.image_received_flag == 1:
+                # Detect sign and highlight it
+
+                sign, processed_image = self.find_sign(self.frame)
+
+                image_topic = self.bridge_object.cv2_to_imgmsg(processed_image, encoding="bgr8")
+                self.image_pub.publish(image_topic)
+
+                if sign is not None:
+                    self.sign_pub.publish(sign)
+
+                self.image_received_flag = 0
+
+
+            key = cv2.waitKey(1)
+            ros_rate.sleep()
+
+    def find_sign(self, frame):
+        # Get sign
+        sign = ""
+
+        # Highlight the sign
+        frame = frame.copy()
+
+        return sign, frame
+
+    def camera_callback(self, data):
+        try:
+            self.frame = self.bridge_object.imgmsg_to_cv2(
+                data, desired_encoding="bgr8")
+            self.image_received_flag = 1
+        except CvBridgeError as e:
+            print(e)
+
+
+    def cleanup(self):
+        print("Shutting down sign detector node")
+        cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    rospy.init_node('sign_detector', anonymous=True)
+    print("Running sign detector")
+    SignDetector()
